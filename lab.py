c6fb33d1dabed8f7466f1a41e4534921ee3c2bd3
tutim2000@gamil.com <tamerk19@meet.mit.edu>
2017-11-06 19:22:38 +0200

    code

---
+++ lab.py
@@ -0,0 +1,16 @@
+class Animal(object):
+    def __int__(self , sound , name , age , favortie_color):
+        self.sound = dound
+        self.name = name
+        self.age = age
+        self.favortie_color = favortie_color
+    def eat(self , food):
+        print("yummy!!"+ self.name + "is eating" + food)
+    def description(self):
+        print(self.name + "is" + self.age + "years old and loves the colore" +  self.favorite_color)
+
+cat = Animal("miaoo, cat, 3, black")
+cat.eat(name , meet)
+print
+
+        
